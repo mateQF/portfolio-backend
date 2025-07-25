9435fa7653d97cfa19175534f88a7112e918f8f1
mateo <mateo.fortu@gmail.com>
2025-07-24 22:13:08 -0300

    refactor(index): improve get/ message

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 import { errorHandler } from './middlewares/errorHandler.js';
-import projectRouter from "./routes/project.routes.js"
+import projectRouter from './routes/project.routes.js';
 
 dotenv.config();
 
@@ -11,13 +11,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(errorHandler);
-app.use("/api/projects", projectRouter)
+app.use('/api/projects', projectRouter);
 
 app.get('/', (_req, res) => {
-    res.send('<h1>MATEO</h1>')
-})
+  res.send('<h1>OK!</h1>');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
-export default app
+export default app;
