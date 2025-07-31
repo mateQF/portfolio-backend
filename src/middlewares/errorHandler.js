f3d036ae3c89ce58be583d92e35596fc50604500
mateo <mateo.fortu@gmail.com>
2025-07-31 09:16:11 -0300

    feature(db) database deploy railway

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,5 +1,4 @@
 import logger from '../utils/logger.js';
-
 // eslint-disable-next-line no-unused-vars
 export function errorHandler(err, req, res, _next) {
   logger.error(`${req.method} ${req.url} - ${err.name}: ${err.message}`);
